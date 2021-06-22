825bee50bc13a291e0e0ecc364f16342a0ea4502
Vincent Peters <vincent@unde.fined.io>
2021-06-22 08:36:11 +0200

    added music + new visual

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,39 @@
 
-$( document ).ready(function() {
-  $('.js-show-info').on('click', (e)=>{
+$(document).ready(function () {
+  $('.js-show-info').on('click', (e) => {
     e.preventDefault();
     $('.js-text').addClass('hidden');
     $('.js-info').removeClass('hidden');
   })
 
-  $('.js-start-ar').on('click', ()=>{
+  $('.js-start-ar').on('click', () => {
     $('.ar').addClass('show')
     $('.js-kaartje').addClass('hidden')
-    $( ".ar" ).load( "ar" );
+    $(".ar").load("ar");
   })
+
+  var isPlaying = false;
+  var first = true;
+  const audio = document.getElementById("audio")
+
+  $('*').click(function () {
+    if (first) {
+      isPlaying = true;
+      $('.js-mute').removeClass('hidden');
+      audio.play();
+      first = false;
+    }
+  });
+
+  $('.js-mute').click(function () {
+    isPlaying ? audio.pause() : audio.play();
+  });
+
+  audio.onplaying = function () {
+    isPlaying = true;
+  };
+
+  audio.onpause = function () {
+    isPlaying = false;
+  };
 });
